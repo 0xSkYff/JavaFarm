024a67769a930d0ae6e49e6dd56428a6968be1f1
0xSkyff <danunix0@gmail.com>
2025-04-10 11:06:11 +0100

    Packages added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Simple farm with animals and crops made in Java 
+
+## Types of crops
+	- Vegetables
+	- Fruits
+	- Cereals
---
+++ src/com/oxskyff/animal/Animal.java
@@ -0,0 +1,34 @@
+package com.oxskyff.animal;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Animal {
+    private String name;
+    private int animalIdentificationNumber;
+    private static int counter;
+
+    public Animal(@NotNull String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        if (!name.isEmpty() && !name.isBlank()) {
+            this.name = name;
+            animalIdentificationNumber = ++counter;
+        } else {
+            System.exit(1); // todo: Add exception handling
+        }
+    }
+
+    public int getAnimalId() {
+        return animalIdentificationNumber;
+    }
+
+    public String toString() {
+        return String.format("[%s] %s, Number %d", getClass().getSimpleName(), name, animalIdentificationNumber);
+    }
+}
